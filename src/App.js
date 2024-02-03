f70768d08a5b2e294083537e470183422d42844b
jayakrishnavamsi24 <jayakrishnavamsi24@gmail.com>
2024-02-03 21:42:58 +0000

    pushed code

--- src/App.js
+++ src/App.js
@@ -1,82 +1,138 @@
 import {Component} from 'react'
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+import LoginRoute from './components/LoginRoute'
+import HomeRoute from './components/HomeRoute'
+import TrendingRoute from './components/TrendingRoute'
+import GamingRoute from './components/GamingRoute'
+import SavedVideosRoute from './components/SavedVideosRoute'
+import VideoItemDetailsRoute from './components/VideoItemDetailsRoute'
+import NotFoundRoute from './components/NotFoundRoute'
+import ProtectedRoute from './components/ProtectedRoute'
+
+import NxtWatchContext from './Context/NxtWatchContext'
+
 import './App.css'
 
-import AppTheme from './context/Theme'
-import Login from './components/Login'
-import Header from './components/Header'
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Trending from './components/Trending'
-import Gaming from './components/Gaming'
-import SavedVideos from './components/SavedVideos'
-import VideoCard from './components/VideoCard'
-import NotFound from './components/NotFound'
-
 class App extends Component {
-  state = {activeTheme: 'light', savedVideos: []}
+  state = {lightTheme: true, likedList: [], dislikedList: [], savedList: []}
 
-  changeTheme = activeTheme => {
-    this.setState({activeTheme})
+  changeTheme = () => {
+    this.setState(prevState => ({lightTheme: !prevState.lightTheme}))
   }
 
-  addSavedVideos = async videoData => {
-    const {savedVideos} = this.state
-    await this.setState({
-      savedVideos: [...savedVideos, videoData],
-    })
+  changedAttributesOnThemeChange = () => {
+    const {lightTheme} = this.state
+
+    let watchLogoImage
+    let failureViewImage
+    let notFoundImage
+
+    if (lightTheme) {
+      watchLogoImage =
+        'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png'
+      failureViewImage =
+        'https://assets.ccbp.in/frontend/react-js/nxt-watch-failure-view-light-theme-img.png'
+      notFoundImage =
+        'https://assets.ccbp.in/frontend/react-js/nxt-watch-not-found-light-theme-img.png'
+    } else {
+      watchLogoImage =
+        'https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-dark-theme-img.png'
+      failureViewImage =
+        'https://assets.ccbp.in/frontend/react-js/nxt-watch-failure-view-dark-theme-img.png'
+      notFoundImage =
+        'https://assets.ccbp.in/frontend/react-js/nxt-watch-not-found-dark-theme-img.png'
+    }
+
+    const watchLogoImageAlt = 'website logo'
+    const failureViewImageAlt = 'failure view'
+    const notFoundImageAlt = 'not found'
+
+    return {
+      watchLogoImage,
+      watchLogoImageAlt,
+      failureViewImage,
+      failureViewImageAlt,
+      notFoundImage,
+      notFoundImageAlt,
+    }
   }
 
-  removeSavedVideos = async videoData => {
-    const {savedVideos} = this.state
-    const filteredVideosList = savedVideos.filter(
-      eachVideo => eachVideo.id !== videoData.id,
-    )
-    await this.setState({savedVideos: filteredVideosList})
+  addAsLikedVideos = id => {
+    const {likedList, dislikedList} = this.state
+    if (dislikedList.includes(id)) {
+      this.setState({dislikedList: dislikedList.filter(each => each !== id)})
+    }
+    if (likedList.includes(id)) {
+      this.setState({likedList: likedList.filter(each => each !== id)})
+    } else {
+      this.setState({likedList: [...likedList, id]})
+    }
+  }
+
+  addAsDislikedVideos = id => {
+    const {likedList, dislikedList} = this.state
+    if (likedList.includes(id)) {
+      this.setState({likedList: likedList.filter(each => each !== id)})
+    }
+    if (dislikedList.includes(id)) {
+      this.setState({dislikedList: dislikedList.filter(each => each !== id)})
+    } else {
+      this.setState({dislikedList: [...dislikedList, id]})
+    }
+  }
+
+  addOrRemoveAsOrFromSavedVideos = videoDetails => {
+    const {savedList} = this.state
+    let savedListIds = []
+    if (savedList.length !== 0) {
+      savedListIds = savedList.map(each => each.id)
+    }
+    if (savedListIds.includes(videoDetails.id)) {
+      this.setState({
+        savedList: savedList.filter(each => each.id !== videoDetails.id),
+      })
+    } else {
+      this.setState({savedList: [...savedList, videoDetails]})
+    }
   }
 
   render() {
-    const {activeTheme, savedVideos} = this.state
-    const bgColor = activeTheme === 'light' ? 'light' : 'dark'
+    const {lightTheme, likedList, dislikedList, savedList} = this.state
 
     return (
-      <AppTheme.Provider
+      <NxtWatchContext.Provider
         value={{
-          activeTheme,
-          savedVideos,
-          addSavedVideos: this.addSavedVideos,
-          removeSavedVideos: this.removeSavedVideos,
+          lightTheme,
           changeTheme: this.changeTheme,
+          changedAttributesOnThemeChange: this.changedAttributesOnThemeChange,
+          likedList,
+          dislikedList,
+          savedList,
+          addAsLikedVideos: this.addAsLikedVideos,
+          addAsDislikedVideos: this.addAsDislikedVideos,
+          addOrRemoveAsOrFromSavedVideos: this.addOrRemoveAsOrFromSavedVideos,
         }}
       >
-        <>
-          <div className="app-container">
-            <Switch>
-              <Route exact path="/login" component={Login} />
-              <>
-                <Header />
-                <div className={`${bgColor} main-frame-container`}>
-                  <Navbar />
-                  <div className="content">
-                    <Switch>
-                      <Route exact path="/" component={Home} />
-                      <Route exact path="/trending" component={Trending} />
-                      <Route exact path="/gaming" component={Gaming} />
-                      <Route
-                        exact
-                        path="/saved-videos"
-                        component={SavedVideos}
-                      />
-                      <Route exact path="/videos/:id" component={VideoCard} />
-                      <Route component={NotFound} />
-                    </Switch>
-                  </div>
-                </div>
-              </>
-            </Switch>
-          </div>
-        </>
-      </AppTheme.Provider>
+        <Switch>
+          <Route exact path="/login" component={LoginRoute} />
+          <ProtectedRoute exact path="/" component={HomeRoute} />
+          <ProtectedRoute exact path="/trending" component={TrendingRoute} />
+          <ProtectedRoute exact path="/gaming" component={GamingRoute} />
+          <ProtectedRoute
+            exact
+            path="/saved-videos"
+            component={SavedVideosRoute}
+          />
+          <ProtectedRoute
+            exact
+            path="/videos/:id"
+            component={VideoItemDetailsRoute}
+          />
+          <Route exact path="/not-found" component={NotFoundRoute} />
+          <Redirect to="/not-found" />
+        </Switch>
+      </NxtWatchContext.Provider>
     )
   }
 }
--- src/components/ContactUs/index.js
+++
@@ -1,3 +0,0 @@
-const ContactUs = () => <p>hi</p>
-
-export default ContactUs
